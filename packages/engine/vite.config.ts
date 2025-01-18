352f3d175138d9e825fc2b9e2508242d3743aba5
Jake Hamilton <jake.hamilton@hey.com>
2025-01-18 10:35:56 -0800

    fix: use externals, map umd globals

--- packages/engine/vite.config.ts
+++ packages/engine/vite.config.ts
@@ -2,14 +2,36 @@ import { defineConfig } from "vite";
 import arraybuffer from "vite-plugin-arraybuffer";
 
 export default defineConfig({
-    plugins: [arraybuffer()],
-    build: {
-        lib: {
-            name: "PixelationEngine",
-            entry: "./src/index.ts",
-            fileName: (format) =>
-                `bundle.${format}.${format === "es" ? "m" : ""}js`,
-            formats: ["es", "umd", "cjs"],
-        },
-    },
+	plugins: [arraybuffer()],
+	build: {
+		lib: {
+			name: "PixelationEngine",
+			entry: "./src/index.ts",
+			fileName: (format) =>
+				`bundle.${format}.${format === "es" ? "m" : ""}js`,
+			formats: ["es", "umd", "cjs"],
+		},
+		rollupOptions: {
+			output: {
+				globals: (id) => {
+					if (id.startsWith("@pixelation/")) {
+						const parts = id.substring(1).split("/");
+						const name =
+							parts[1][0].toUpperCase() + parts[1].substring(1);
+
+						return `Pixelation${name}`;
+					} else if (id.startsWith("@")) {
+						const parts = id.substring(1).split("/");
+
+						return parts[1] ?? parts[0];
+					} else {
+						return id;
+					}
+				},
+			},
+			external: (id) => {
+				return id.startsWith("@pixelation/");
+			},
+		},
+	},
 });
--- packages/scene/vite.config.ts
+++ packages/scene/vite.config.ts
@@ -9,5 +9,27 @@ export default defineConfig({
 				`bundle.${format}.${format === "es" ? "m" : ""}js`,
 			formats: ["es", "umd", "cjs"],
 		},
+		rollupOptions: {
+			output: {
+				globals: (id) => {
+					if (id.startsWith("@pixelation/")) {
+						const parts = id.substring(1).split("/");
+						const name =
+							parts[1][0].toUpperCase() + parts[1].substring(1);
+
+						return `Pixelation${name}`;
+					} else if (id.startsWith("@")) {
+						const parts = id.substring(1).split("/");
+
+						return parts[1] ?? parts[0];
+					} else {
+						return id;
+					}
+				},
+			},
+			external: (id) => {
+				return id.startsWith("@pixelation/");
+			},
+		},
 	},
 });
